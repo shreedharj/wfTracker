4766bee722b3c41144c856bafd95735b82020513
Shivam Adeshara <shivam@Shivams-MacBook-Pro.local>
2022-01-16 03:30:37 -0800

    removed nasa file

--- nasa.py
+++
@@ -1,7 +0,0 @@
-import requests
-r = requests.get("https://eonet.gsfc.nasa.gov/api/v2.1/events")
-events = r.json()["events"]
-#print(events)
-for i in events:
-    if i[0].id == 8:
-        print(i)
